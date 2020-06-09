5010405ec5d4df2de5e90ced090b0235f3179507
lys122519 <514955048@qq.com>
2020-06-09 10:24:48 +0800

    TTMS

---
+++ src/View/SalesAnalysis_UI.h
@@ -0,0 +1,9 @@
+#ifndef SALESANALYSIS_UI_H_
+#define SALESANALYSIS_UI_H_
+
+#include "../Service/SalesAnalysis.h"
+#include "../Service/Sale.h"
+#include "../Service/Play.h"
+#include "../Service/Account.h"
+#include "../Common/list.h"
+#endif
