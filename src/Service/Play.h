fb341f09cb13d8bdb177935e883167df38b241af
aPlikethis <yourhand1025@163.com>
2020-06-09 10:57:26 +0800

    TTMs

--- src/Service/Play.h
+++ src/Service/Play.h
@@ -5,7 +5,7 @@
 /* 获取剧目信息 */
 play_list_t Play_Srv_FetchAll(play_list_t list);
 /* 查询剧目 */
-play_node_t Play_Srv_FetchByID(play_list_t list);
+play_list_t Play_Srv_FetchByID(play_list_t list, int id);
 
 
 #endif
