fc522ba453d97e2c9f003809a2c47d194a1d9231
aPlikethis <yourhand1025@163.com>
2020-06-10 11:34:50 +0800

    TTMS

--- src/Service/Ticket.h
+++ src/Service/Ticket.h
@@ -18,7 +18,7 @@ int Ticket_Srv_FetchByID(int id, ticket_t *buf);
 
 //标识符：TTMS_SCU_Schedule_Srv_FetchByID
 //功能: 根据ID获取演出计划
-int Schedule_Srv_FetchByID(int id,schedule_t*buf );
+int Schedule_Srv_FetchByID(int id,schedule_t *buf );
 
 
 //票状态类型定义
--- src/View/Ticket_UI.c
+++ src/View/Ticket_UI.c
@@ -8,14 +8,76 @@
 #include "../Service/studio.h"
 #include "../Service/Schedule.h"
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 //标识符：TTMS_SCU_Ticket_UI_MgtEnt
 //功能：显示与参数对应的演出计划信息
 void Ticket_UI_MgtEntry(int schedule_id)
 {
-    Play_Srv_FetchByID( schedule_id )
+    char choice;
 
+    //根据演出计划ID获取演出计划信息
+    //从获取的信息中，使用剧目ID获取该剧目信息
+    schedule_t *need_schedule;
+    play_t *need_play;
+
+    need_schedule = (schedule_t *)malloc(sizeof(schedule_t));
+    need_play = (play_t *)malloc(sizeof(play_t));
+    
+    Schedule_Srv_FetchByID(schedule_id, need_schedule);
+    Play_Srv_FetchByID( need_schedule->play_id, need_play);
+    
+    
+    
+    
+    do{
+        printf(
+                "\n==================================================================================================\n");
+        printf(
+                "*********************************************票 信 息*********************************************\n");
+		printf("             剧目名称             演出厅编号             演出日期             演出时间             \n\n");
+		
+
+		printf("           %-6s                 %.2d                %d/%.2d/%.2d            %.2d:%.2d:%.2d\n\n\n",
+                need_play->name,need_schedule->id,
+                need_schedule->date.year,need_schedule->date.month,need_schedule->date.day,
+                need_schedule->time.hour,need_schedule->time.minute,need_schedule->time.second);
+
+
+		printf("**************************************************************************************************\n");
+		printf("          |    [A]     生成该演出票    |    [D]    重新生成演出票    |     [R]     返回上层       |\n");
+		printf("\n==================================================================================================\n");
+		printf("请输入您的选择：");
+		fflush(stdin);
+		scanf("%c",&choice);
+		getchar();
+        if( choice=='a' || choice == 'A' || choice == 'd' || choice == 'D' || choice == 'R' || choice == 'r')
+        { 
+		    switch(choice)
+            {
+		        case 'a':
+	        	case 'A':
+		         	if (Ticke_Srv_GenBatch(schedule_id, need_schedule->play_id))
+			        	printf("生成成功!\n");
+			     else
+				        printf("生成失败!\n");
+			        break;
+		        case 'd':
+		        case 'D':
+			        if(Ticket_Sry_DeleteBatch(schedule_id) && Ticker_Srv_GenBatch(schedule_id, need_schedule->play_id))
+				        printf("重新生成成功");
+			        else
+				        printf("重新生成失败");
+			        break;
+            }   
+        }
+        else
+        {
+           printf("未知操作，请重新输入"); 
+        }
+	}while (choice != 'r' && choice != 'R');
+	system("cls");
 
 }
 //标识符：TTMS_SCU_Ticket_UI_Qry
