f3317573cc42edd61a6a965ed1825acc94a791f1
lys122519 <514955048@qq.com>
2020-06-10 14:25:47 +0800

    TTMS

--- src/Persistence/Sale_Persist.h
+++ src/Persistence/Sale_Persist.h
@@ -7,9 +7,8 @@
 int Sale_Perst_lnsert(const sale_t *data);
 int Sale_Perst_DeleteByID(int saleID);
 //根据用户ID载入给定时间区间内的销售记录
-int Sale_Perst_SelectByUsrID(sale_list_t list,int usrID,user_date_t stDate,user_date_t endDate);
+int Sale_Perst_SelectByUsrID(sale_list_t list,int usrID,ttms_date_t stDate,ttms_date_t endDate);
 //根据时间区间载入用户ID的销售记录
-int Sale_Perst_SelectByDate(sale_list_t list,
-		user_date_t stDate, user_date_t endDate);
+int Sale_Perst_SelectByDate(sale_list_t list,ttms_date_t stDate, ttms_date_t endDate);
 
 #endif
