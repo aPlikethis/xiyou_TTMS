388bb13ac8c0726384e2220fc08e929a7100d7d3
lys122519 <514955048@qq.com>
2020-06-11 16:11:44 +0800

    TTMS

--- src/Service/StaSales.c
+++ src/Service/StaSales.c
@@ -4,7 +4,7 @@
 #include "../Persistence/Sale_Persist.h"
 #include "../Persistence/Account_Persist.h"
 
-//根据ID获取销售额统计
+//根据员工ID获取销售额统计
 int StaSales_Srv_CompSaleVal(int usrID, ttms_date_t stDate, ttms_date_t endDate)
 {
     int amount = 0;//存储销售额
