6293ae1e945b07b1a07b22feae3fe925facc5b90
lys122519 <514955048@qq.com>
2020-06-14 13:08:57 +0800

    TTMS

--- src/Persistence/SalesAnalysis_Persist.h
+++ src/Persistence/SalesAnalysis_Persist.h
@@ -7,6 +7,6 @@
 
 int Salesanalysis_Perst_Insert(const salesanalysis_t *data);
 int SalesAnalysis_Perst_SelectAll(salesanalysis_list_t list);
-int Sale_Perst_SelectALL(sale_list_t list);
+
 
 #endif
